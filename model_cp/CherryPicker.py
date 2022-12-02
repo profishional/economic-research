8cd27a7f33b3fb755562f87dc9dee073aeb65796
profishional <profishcrypto@gmail.com>
2022-12-02 17:28:52 +1100

    cp object for external simluation

---
+++ model_cp/CherryPicker.py
@@ -0,0 +1,15 @@
+from .parts.utils import *
+
+"""
+    This is a Cherry Picker (CP) object for external models.
+"""
+
+class CherryPicker():
+
+    def __init__(self, chain_id: int) -> None:
+        self.chain_id = chain_id
+
+    def get_probability(QoS: float, latency: float):
+        pass
+
+
